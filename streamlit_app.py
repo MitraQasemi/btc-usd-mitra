9ca1385e8701f0e4742a9fbc1e6de63acd729276
MitraQasemi <109904083+MitraQasemi@users.noreply.github.com>
2025-05-31 10:19:45 +0000

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ if st.button("Run Forecast"):
         })
 
         # Streamlit chart
-        st.line_chart(df_combined,height=100)
+        st.line_chart(df_combined)
 
     except Exception as e:
         st.error(f"❌ Forecasting failed: {e}")
